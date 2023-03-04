70eb91863a243168ced0059778229fc17cba844d
Lucas Anderson <lucasanderson1999@gmail.com>
2023-03-03 23:56:55 -0500

    make toggleable

--- AbyssPitFinder.cs
+++ AbyssPitFinder.cs
@@ -2,23 +2,42 @@
 using HutongGames.PlayMaker;
 using Modding;
 
-namespace AbyssPitRemover {
-    internal class AbyssPitFinder : MonoBehaviour {
-        private bool removed = false;
-        private PlayMakerFSM attackChoices;
+// i wonder if anyone will ever read this code
+public class AbyssPitFinder : MonoBehaviour {
+    private bool removed = false;
+    private PlayMakerFSM attackChoices;
+    private GameObject abyssPit = null;
+    private GameObject abs = null;
 
-        private void Update() {
-            GameObject abs = GameObject.Find("Absolute Radiance");
+    public void Update() {
+        if (abs == null) {
+            abs = GameObject.Find("Absolute Radiance");
+        }
 
-            if (!removed && abs != null) {
-                attackChoices = abs.LocateMyFSM("Attack Choices");
-                
-                if (attackChoices != null && attackChoices.ActiveStateName == "A2 End" && abs.transform.position.y >= 150f) {
-                    Destroy(GameObject.Find("Abyss Pit"));
+        if (!removed && abs != null) {
+            attackChoices = abs.LocateMyFSM("Attack Choices");
+            
+            if (attackChoices != null && attackChoices.ActiveStateName == "A2 End" && abs.transform.position.y >= 150f) {
+                abyssPit = GameObject.Find("Abyss Pit");
+
+                if (abyssPit) {
+                    abyssPit.SetActive(false);
+                    foreach (Component comp in abyssPit.GetComponents(typeof(Component))) {
+                        Modding.Logger.Log(comp.gameObject.name);
+                        Modding.Logger.Log(comp.GetType());
+                    }
                     removed = true;
                     Modding.Logger.Log("Destroyed Abyss Pit");
                 }
             }
         }
+    }
+
+    public void Unload() {
+        removed = false;
+        if (abyssPit) {
+            abyssPit.SetActive(true);
+        }
+        abs = null;
     }
 }
--- AbyssPitRemover.cs
+++ AbyssPitRemover.cs
@@ -4,33 +4,44 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using USceneManager = UnityEngine.SceneManagement.SceneManager;
 
-namespace AbyssPitRemover {
-    public class AbyssPitRemover : Mod {
-        internal static AbyssPitRemover instance;
+public class AbyssPitRemover : Mod, ITogglableMod {
+    internal static AbyssPitRemover instance;
 
-        public AbyssPitRemover() : base("AbyssPitRemover") {
-           instance = this;
-        }
+    public AbyssPitRemover() : base("Abyss Pit Remover") {
+        instance = this;
+    }
 
-        public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects) {
-            USceneManager.activeSceneChanged += CheckForRadiance;
-            Log("Initialized.");
-        }
+    public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects) {
+        Log("Initializing");
 
-        public override string GetVersion() {
-            return "1.0.0.0";
-        }
-
-        public void Unload() {
-            USceneManager.activeSceneChanged -= CheckForRadiance;
-        }
-
-        private static void CheckForRadiance(Scene from, Scene to) {
-            if (to.name != "GG_Radiance") {
-                return;
-            }
-
+        USceneManager.activeSceneChanged += CheckForRadianceFight;
+        if (USceneManager.GetActiveScene().name == "GG_Radiance") {
             new GameObject("AbyssPitFinder", typeof(AbyssPitFinder));
         }
+
+        Log("Initialized");
+    }
+
+    public override string GetVersion() => GetType().Assembly.GetName().Version.ToString();
+
+    public void Unload() {
+        USceneManager.activeSceneChanged -= CheckForRadianceFight;
+        GameObject abyssPitFinder = GameObject.Find("AbyssPitFinder");
+        if (abyssPitFinder) {
+            abyssPitFinder.GetComponent<AbyssPitFinder>().Unload();
+            GameObject.DestroyImmediate(abyssPitFinder);
+        }
+    }
+
+    private static void CheckForRadianceFight(Scene from, Scene to) {
+        if (from.name == "GG_Radiance") {
+            GameObject.DestroyImmediate(GameObject.Find("AbyssPitFinder"));
+        }
+
+        if (to.name != "GG_Radiance") {
+            return;
+        }
+
+        new GameObject("AbyssPitFinder", typeof(AbyssPitFinder));
     }
 }
